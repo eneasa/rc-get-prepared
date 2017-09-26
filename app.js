fcbd75e09d3cb685d2f369f1f97fe7c71567ba02
Juan EA Garcia <juan.garcia@deepend.com.au>
2017-09-26 18:49:40 +1000

    changes in json

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function GetGoogleSheetRowsByWorkSheetIDRequest(googleSheetNumber) {
 }
 
 // this is the worksheet, the first worksheet has ID=2, second worksheet ID=3 and so on 
-function GetKenticoCloudRows(path) {
+function GetKenticoCloudRowsByPath(path) {
     
     // this is the sheet ID found in the URL
     var projectID = "112fe4bd-5bac-41f2-8ba1-719f736a3017";
@@ -22,6 +22,19 @@ function GetKenticoCloudRows(path) {
     
 }
 
+// this is the worksheet, the first worksheet has ID=2, second worksheet ID=3 and so on 
+function GetKenticoCloudRowsbyType(type) {
+    
+    // this is the sheet ID found in the URL
+    var projectID = "112fe4bd-5bac-41f2-8ba1-719f736a3017";
+
+    var url = "https://deliver.kenticocloud.com/" + projectID + "/items?system.type[in]=" + type + "";
+    
+    return $.getJSON(url);
+    
+}
+
+
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -29,9 +42,16 @@ function getParameterByName(name, url) {
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+    return decodeURIComponent(results[2].replace(/\+/g, ""));
 }
 
 function goBack() {
     window.history.back();
+}
+
+function GetImagePath(imageField){
+
+    var defaultImage = "http://placehold.it/42x42"
+    return imageField[0] ? imageField[0].url : defaultImage;
+    
 }
